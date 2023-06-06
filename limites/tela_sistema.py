296cb68e311ffffebbda12a40c0a414af20d29f5
Rafaela Borges <rafasborgesw@gmail.com>
2023-06-05 21:28:10 -0300

    Tela sistema - PySimpleGUI

--- limites/tela_sistema.py
+++ limites/tela_sistema.py
@@ -1,6 +1,6 @@
 from limites.tela import Tela
 
-
+'''
 class TelaSistema(Tela):
 
     def tela_opcoes(self):
@@ -26,4 +26,55 @@ class TelaSistema(Tela):
         print("-------------------------------------")
         opcao = self.le_num_inteiro("Escolha a opção: ", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
         return opcao
+'''
 
+import PySimpleGUI as sg
+
+class TelaSistema(Tela):
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+
+# fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+# precisa chamar self.init_components() aqui para o caso de chamar essa janela uma 2a vez. Não é possível reusar layouts de janelas depois de fechadas.
+    def tela_opcoes(self):
+        self.init_components()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['5']:
+            opcao = 5
+        if values['6']:
+            opcao = 6
+        # cobre os casos de voltar, não clicar em nada e fechar janela, ou clicar cancelar
+        if values['0'] or button in (None,'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+
+    def close(self):
+        self.__window.Close()
+
+    def init_components(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('LightBlue2')
+        layout = [
+            [sg.Text('Bem vindo ao Sistema de Restaurante!', font=("Helvica",25))],
+            [sg.Text('Escolha sua opção', font=("Helvica",15))],
+            [sg.Radio('Mesas',"RD1", key='1')],
+            [sg.Radio('Itens do cardápio',"RD1", key='2')],
+            [sg.Radio('Funcionários',"RD1", key='3')],
+            [sg.Radio('Clientes',"RD1", key='4')],
+            [sg.Radio('Reservas',"RD1", key='5')],
+            [sg.Radio('Pedidos',"RD1", key='6')],
+            [sg.Radio('Finalizar sistema',"RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de Restaurante').Layout(layout)
