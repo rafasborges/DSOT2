cd1870588d5ea49d3ee1653f8051c2a5ae5ba266
Rafaela Borges <rafasborgesw@gmail.com>
2023-06-08 18:51:26 -0300

    Tela mesa - PySimpleGUI - cliente

--- limites/tela_cliente.py
+++ limites/tela_cliente.py
@@ -1,6 +1,6 @@
 from limites.tela import Tela
 
-
+'''
 class TelaCliente(Tela):
                     
     def tela_opcoes(self):
@@ -52,4 +52,103 @@ class TelaCliente(Tela):
             except ValueError:
                 print("\nInsira um valor válido!")
                 print("\n")
+'''
+import PySimpleGUI as sg
+
+
+class TelaCliente():
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
+
+# fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+    def tela_opcoes(self):
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+          opcao = 1
+        if values['2']:
+          opcao = 2
+        if values['3']:
+          opcao = 3
+        if values['4']:
+          opcao = 4
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0'] or button in (None, 'Cancelar'):
+          opcao = 0
+        self.close()
+        return opcao
+
+    def init_opcoes(self):
+      # sg.theme_previewer()
+      sg.ChangeLookAndFeel('DarkTeal4')
+      layout = [
+        [sg.Text('-------- CLIENTES ----------', font=("Helvica", 25))],
+        [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+        [sg.Radio('Incluir Cliente', "RD1", key='1')],
+        [sg.Radio('Alterar Cliente', "RD1", key='2')],
+        [sg.Radio('Listar Cliente', "RD1", key='3')],
+        [sg.Radio('Excluir Cliente', "RD1", key='4')],
+        [sg.Radio('Retornar', "RD1", key='0')],
+        [sg.Button('Cancelar'), sg.Cancel('Confirmar')]
+      ]
+      self.__window = sg.Window('Sistema de livros').Layout(layout)
+
+    # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+    # opção de tratamento: adicionar um if e só coletar nome e telefone se o button é 'Confirmar'
+    def pega_dados_amigo(self):
+      sg.ChangeLookAndFeel('DarkTeal4')
+      layout = [
+        [sg.Text('-------- DADOS AMIGO ----------', font=("Helvica", 25))],
+        [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+        [sg.Text('Telefone:', size=(15, 1)), sg.InputText('', key='telefone')],
+        [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+      ]
+      self.__window = sg.Window('Sistema de livros').Layout(layout)
+
+      button, values = self.open()
+      nome = values['nome']
+      telefone = values['telefone']
+      cpf = values['cpf']
+
+      self.close()
+      return {"nome": nome, "telefone": telefone, "cpf": cpf}
+
+    # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+    def mostra_amigo(self, dados_amigo):
+      string_todos_amigos = ""
+      for dado in dados_amigo:
+        string_todos_amigos = string_todos_amigos + "NOME DO AMIGO: " + dado["nome"] + '\n'
+        string_todos_amigos = string_todos_amigos + "FONE DO AMIGO: " + str(dado["telefone"]) + '\n'
+        string_todos_amigos = string_todos_amigos + "CPF DO AMIGO: " + str(dado["cpf"]) + '\n\n'
+
+      sg.Popup('-------- LISTA DE AMIGOS ----------', string_todos_amigos)
+
+    # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+    def seleciona_amigo(self):
+      sg.ChangeLookAndFeel('DarkTeal4')
+      layout = [
+        [sg.Text('-------- SELECIONAR AMIGO ----------', font=("Helvica", 25))],
+        [sg.Text('Digite o CPF do amigo que deseja selecionar:', font=("Helvica", 15))],
+        [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+      ]
+      self.__window = sg.Window('Seleciona amigo').Layout(layout)
+
+      button, values = self.open()
+      cpf = values['cpf']
+      self.close()
+      return cpf
+
+    def mostra_mensagem(self, msg):
+      sg.popup("", msg)
+
+    def close(self):
+      self.__window.Close()
+
+    def open(self):
+      button, values = self.__window.Read()
+      return button, values
 
