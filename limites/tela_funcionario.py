792e92fa160946e8007d021552ef9ae5086e464c
Rafaela Borges <rafasborgesw@gmail.com>
2023-06-13 19:18:53 -0300

    tela funcionario

--- limites/tela_funcionario.py
+++ limites/tela_funcionario.py
@@ -1,57 +1,4 @@
 from limites.tela import Tela
-
-"""
-class TelaFuncionario(Tela):
-
-    def tela_opcoes(self):
-        print("--------- CADASTRO FUNCIONÁRIO --------- ")
-        print(" 1 - Incluir Funcionário")
-        print(" 2 - Alterar Funcionário")
-        print(" 3 - Excluir Funcionário")
-        print(" 4 - Listar Funcionário")
-        print(" 5 - Ver salário Funcionário")
-        print(" 6 - Voltar")
-        opcao = self.le_num_inteiro("Escolha a opção: ", [0, 1, 2, 3, 4, 5, 6])
-        return opcao
-
-    def pega_dados_funcionario(self):
-        print("-------- DADOS FUNCIONARIO ----------")
-        while True:
-            try:
-                nome = input("Nome: ")
-                cpf = input("CPF: ")
-                salario = float(input("Salário: "))
-                if ((self.checa_valor(nome) == True) or
-                    (not isinstance(salario, (int, float)) or
-                    len(cpf) != 11) or
-                    salario < 0):
-                    raise ValueError
-                return {"nome": nome.upper(), "cpf": cpf, "salario": salario}
-            except ValueError:
-                print("Dados incorretos! O CPF deve conter 11 dígitos! Utilize apenas strings para o nome e números decimais positivos para o salário!")
-    
-
-    def mostra_funcionario(self, dados_funcionario):
-        try:
-            print("NOME DO FUNCIONARIO: ", dados_funcionario["nome"].upper())
-            print("CPF DO FUNCIONARIO: ", dados_funcionario["cpf"])
-            print("SALÁRIO DO FUNCIONARIO: ", dados_funcionario["salario"])
-        except KeyError as e:
-            print("Erro ao exibir dados do funcionário:", str(e))
-        print("\n")
-
-    def seleciona_funcionario(self):
-        while True:
-            nome_lido = input("Nome do funcionario que deseja selecionar: ").upper()
-            try:
-                nome = str(nome_lido)
-                if not isinstance(nome, str):
-                    raise ValueError
-                return nome
-            except ValueError:
-                print("\nInsira um valor válido!")
-                print("\n")
-"""
 import PySimpleGUI as sg
 
 class TelaFuncionario(Tela):
@@ -125,14 +72,17 @@ class TelaFuncionario(Tela):
 
     # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
     def mostra_funcionario(self, dados_cliente):
-      string_todos_funcionarios = ""
-      for dado in dados_funcionario:
-        string_todos_funcionarios = string_todos_funcionarios + "NOME DO FUNCIONARIO: " + dados_funcionario["nome"] + '\n'
-        string_todos_funcionarios = string_todos_funcionarios + "CPF DO FUNCIONARIO: " + str(dados_funcionario["cpf"]) + '\n'
-        string_todos_funcionarios = string_todos_funcionarios + "SALÁRIO DO FUNCIONARIO: " + str(dados_funcionario["salario"]) + '\n'
+        try:
+            string_todos_funcionarios = ""
+            for dado in dados_funcionario:
+                string_todos_funcionarios = string_todos_funcionarios + "NOME DO FUNCIONARIO: " + dados_funcionario["nome"] + '\n'
+                string_todos_funcionarios = string_todos_funcionarios + "CPF DO FUNCIONARIO: " + str(dados_funcionario["cpf"]) + '\n'
+                string_todos_funcionarios = string_todos_funcionarios + "SALÁRIO DO FUNCIONARIO: " + str(dados_funcionario["salario"]) + '\n'
 
 
-      sg.Popup('-------- LISTA DE FUNCIONARIOS ----------', string_todos_clientes)
+            sg.Popup('-------- LISTA DE FUNCIONARIOS ----------', string_todos_clientes)
+            except KeyError as e:
+                sg.Popup("Erro ao exibir dados de funcionários: ", str(e))
       #self.__window = sg.Window('Lista de Funcionários').Layout(layout)
     # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
     def seleciona_funcionario(self):
